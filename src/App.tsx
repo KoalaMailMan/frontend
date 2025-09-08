784b173bfd5bd79368328eeaeb53c0b4895da2b6
mamonde456 <mamonde456@gmail.com>
2025-09-08 15:42:36 +0900

    fix: home page 컴포넌트로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import MainSection from "./feature/home/components/MainSection";
-import ScrollAnimation from "./feature/home/components/ScrollAnimation";
+import HomePage from "./feature/home/components/HomePage";
 
 function App() {
   return (
     <>
-      <header></header>
-      <main>
-        <div className="min-h-screen relative overflow-hidden">
-          <section className="min-h-screen flex flex-col items-center justify-center px-4 relative">
-            <div
-              className="relative z-20 text-center"
-              style={{
-                transform: `translateY(${Math.min(scrollY * 0.1, 20)}px)`,
-              }}
-            >
-              <MainSection />
-            </div>
-            <ScrollAnimation />
-          </section>
-        </div>
-        <section></section>
-      </main>
+      <HomePage />
     </>
   );
 }
