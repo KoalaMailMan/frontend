c6aec7aba4ef003649c91dea4ee38a6588a2c0aa
mamonde456 <mamonde456@gmail.com>
2025-09-23 17:07:37 +0900

    feat: 만다라트 대시보드 생성 및 수정 api 추가

---
+++ src/feature/mandala/api/updateMandala.ts
@@ -0,0 +1,16 @@
+import { apiClient } from "@/lib/api/client";
+
+export const updateMandala = async (accessToken: string, data: any) => {
+  const MANDALA_URL = "/api/mandalart";
+  try {
+    const res = await apiClient.put(MANDALA_URL, data, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      credentials: "include",
+    });
+    return res;
+  } catch (error) {
+    console.error("updateMandala failed:", error);
+  }
+};
