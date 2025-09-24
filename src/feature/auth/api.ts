eb1a6a30c1c77e85b93c3e2098d144eb40e9c22e
mamonde456 <mamonde456@gmail.com>
2025-09-24 13:36:38 +0900

    feat: 사용자 정보 조회 api 추가

--- src/feature/auth/api.ts
+++ src/feature/auth/api.ts
@@ -1,4 +1,5 @@
 import { apiClient } from "@/lib/api/client";
+import type { UserType } from "./type";
 
 export const logouAPI = async () => {
   const LOGOUT_URL = "/api/auth/logout";
@@ -26,6 +27,23 @@ export const refreshTokenAPI = async () => {
     if (!accessToken) throw new Error("refresh failed");
 
     return accessToken;
+  } catch (error) {
+    console.error("refresh failed:", error);
+  }
+};
+
+export const getUserProfileAPI = async (
+  accessToken: string
+): Promise<UserType | undefined> => {
+  const USER_URL = "/api/user";
+  try {
+    const res = await apiClient.get(USER_URL, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      credentials: "include",
+    });
+    return res;
   } catch (error) {
     console.error("refresh failed:", error);
   }
