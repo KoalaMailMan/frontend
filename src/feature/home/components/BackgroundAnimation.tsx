6edd5f9e7bff71df950c77cd6952cd1257272bc5
mamonde456 <mamonde456@gmail.com>
2025-09-25 10:11:02 +0900

    feat: background animation 이미지 수정 & 스타일 수정

--- src/feature/home/components/BackgroundAnimation.tsx
+++ src/feature/home/components/BackgroundAnimation.tsx
@@ -1,27 +1,29 @@
-import koalaPixelImage from "../../../assets/default_koala.png";
+import koalaRedImage from "@/assets/mini_red_koala.png";
+import koalaGreenImage from "@/assets/mini_green_koala.png";
+import koalaPurpleImage from "@/assets/mini_purple_koala.png";
 
 export default function BackgroundAnimation() {
   return (
     <div className="absolute inset-0 overflow-hidden pointer-events-none">
       <div className="flying-koala">
         <img
-          src={koalaPixelImage}
+          src={koalaRedImage}
           alt="날아가는 코알라"
-          className="w-12 h-12 pixelated opacity-60"
+          className="w-20 h-20 pixelated opacity-85"
         />
       </div>
       <div className="flying-koala" style={{ animationDelay: "3s" }}>
         <img
-          src={koalaPixelImage}
+          src={koalaGreenImage}
           alt="날아가는 코알라"
-          className="w-8 h-8 pixelated opacity-40"
+          className="w-18 h-18 pixelated opacity-80"
         />
       </div>
       <div className="flying-koala" style={{ animationDelay: "6s" }}>
         <img
-          src={koalaPixelImage}
+          src={koalaPurpleImage}
           alt="날아가는 코알라"
-          className="w-10 h-10 pixelated opacity-50"
+          className="w-16 h-16 pixelated opacity-90"
         />
       </div>
     </div>
