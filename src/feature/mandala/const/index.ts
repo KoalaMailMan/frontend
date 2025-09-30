a678c72399d2e9afec75cd68b748c5102eef6854
mamonde456 <mamonde456@gmail.com>
2025-09-30 13:39:49 +0900

    feat: 리마인드 알림 주기 const 추가

---
+++ src/feature/mandala/const/index.ts
@@ -0,0 +1,8 @@
+export const IntervalType: { [index: string]: string } = {
+  "1week": "ONE_WEEK",
+  "2week": "TWO_WEEKS",
+  "1month": "ONE_MONTH",
+  "2month": "TWO_MONTHS",
+  "3month": "THREE_MONTHS",
+  "6month": "SIX_MONTHS",
+};
