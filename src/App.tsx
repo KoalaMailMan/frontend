c7785200941cf5af6223801fc261525ce8fdbf3b
mamonde456 <mamonde456@gmail.com>
2025-10-14 15:50:47 +0900

    refactor: header 컴포넌트 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "./feature/ui/Sonner";
 import { toast } from "sonner";
+import Header from "./shared/\bcomponents/header/Header";
 
 const queryClient = new QueryClient();
 
@@ -52,6 +53,8 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Header currentTheme={currentTheme} onThemeChange={updateCurrentTheme} />
+
       {wasLoggedIn ? (
         <MandalaBoard
           currentTheme={currentTheme}
