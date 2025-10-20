c17a058bb6f86dba3876efd045552bf4b358fd0f
mamonde456 <mamonde456@gmail.com>
2025-10-20 15:23:24 +0900

    feat: SEO를 위한 파일 설정 추가
    - index.html 에 meta 태그 추가 및 JSON-LD 추가
    - 웹 크롤링을 위해 robots/sitemap 추가
    - html 프리 렌더링을 위한 플러그인 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { imagetools } from "vite-imagetools";
+import vitePrerender from "vite-plugin-prerender";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), imagetools()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    imagetools(),
+    vitePrerender({
+      staticDir: path.join(__dirname, "dist"),
+      routes: ["/"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
