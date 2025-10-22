fd93b2f53fd9a191621b05c5cf26625b88b54659
mamonde456 <mamonde456@gmail.com>
2025-10-22 14:53:37 +0900

    fix: vite-plugin-prerender 삭제
    - 프리렌더링 빌드 시 import ESM 오류 발생.
    - 콘텐츠 사이트가 아니므로 프리렌더링의 필요성이 적기 때문에 삭제 결정

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { imagetools } from "vite-imagetools";
-import vitePrerender from "vite-plugin-prerender";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    imagetools(),
-    vitePrerender({
-      staticDir: path.join(__dirname, "dist"),
-      routes: ["/"],
-    }),
-  ],
+  plugins: [react(), tailwindcss(), imagetools()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
