50ed9dc464d648d727d269123f31b68c5c4044e6
mamonde456 <mamonde456@gmail.com>
2025-10-23 14:19:50 +0900

    fix: access token을 헤더에 추가 및 url 파싱 로직 추가

--- src/feature/mandala/components/MandalaModal.tsx
+++ src/feature/mandala/components/MandalaModal.tsx
@@ -1,8 +1,8 @@
 import { useMandalaStore, type SubGoal } from "@/lib/stores/mandalaStore";
 import MandalaContainer from "./MandalaContainer";
 import { Fragment } from "react/jsx-runtime";
-import { useEffect, useRef } from "react";
-import { Lightbulb, Loader, X } from "lucide-react";
+import { useCallback, useEffect, useRef } from "react";
+import { Lightbulb, Loader } from "lucide-react";
 
 import koalaImage from "@/assets/common/default_koala.png";
 import { createPortal } from "react-dom";
@@ -12,6 +12,15 @@ import { getGridClasses } from "../utills/css";
 import { toast } from "sonner";
 import { useTutorialStore } from "@/lib/stores/tutorialStore";
 import useSSERecommendation from "../hooks/useSSERecommendation";
+import RecommendIcon from "./icon/RecommendIcon";
+import X from "@/feature/tutorial/components/icons/X";
+import { useAuthStore } from "@/lib/stores/authStore";
+import {
+  handleLogout,
+  reissueWithRefreshToken,
+  shouldAttemptRefresh,
+} from "@/feature/auth/service";
+import { APIWithRetry } from "@/feature/auth/\butils";
 
 type Props = {
   isModalVisible: boolean;
@@ -29,15 +38,30 @@ export default function MandalaModal({
   onCancelEdit,
 }: Props) {
   // modal DetailedGoalRecommendationBox 컴포넌트 상태 관리
+  const accessToken = useAuthStore((state) => state.accessToken);
   const isOnboardingOpen = useTutorialStore((state) => state.isOnboardingOpen);
   const updateSubsCell = useMandalaStore((state) => state.updateSubsCell);
   const modalCellId = useMandalaStore((state) => state.modalCellId);
   const count = useRef(0);
 
+  const getAccessToken = useCallback(async () => {
+    if (accessToken) return accessToken;
+    if (shouldAttemptRefresh()) {
+      const success = await APIWithRetry(reissueWithRefreshToken);
+      if (!success) {
+        handleLogout();
+        toast("세션 종료로 인해 처음 화면으로 돌아갑니다.");
+      } else {
+        return accessToken;
+      }
+    }
+  }, [accessToken]);
+
   const { startStream, stopStream, recommendation, isStreaming } =
     useSSERecommendation({
       goal: item[0].content,
       count: count.current,
+      getAccessToken,
       onComplete: (items) => {
         console.log("완료! 총", items.length, "개");
         toast.success(`목표 추천 완료되었습니다!`);
@@ -121,13 +145,13 @@ export default function MandalaModal({
         className="bg-white rounded-lg shadow-2xl max-w-2xl w-full max-h-[90vh] overflow-y-auto"
         onClick={(e) => e.stopPropagation()} // 모달 내용 클릭 시 이벤트 버블링 방지
       >
-        <div className="flex items-center justify-between p-6 border-b">
-          <div className="flex items-center gap-3">
-            <img src={koalaImage} alt="코알라" className="w-8 h-8" />
-            <h2 className="text-xl font-semibold">세부 목표 설정</h2>
-          </div>
-          <Button variant="ghost" size="icon" onClick={handleModalClose}>
-            <X className="h-4 w-4" />
+        <div className="flex items-center justify-end p-6 border-b">
+          {/* <div className="flex items-center gap-3"> */}
+          {/* <img src={koalaImage} alt="코알라" className="w-8 h-8" /> */}
+          {/* <h2 className="text-xl font-semibold">세부 목표 설정</h2> */}
+          {/* </div> */}
+          <Button variant="none" size="icon" onClick={handleModalClose}>
+            <X size={22} stroke="#2A2D3A" />
           </Button>
         </div>
         <div className="p-6 text-center border-b">
@@ -162,7 +186,7 @@ export default function MandalaModal({
                             className={cn(
                               getGridClasses(index),
                               isCenter
-                                ? "opacity-50 bg-primary/20 border-primary font-medium text-primary"
+                                ? " bg-[#DF6556]/20 border-[#DF6556] font-medium text-primary"
                                 : ""
                             )}
                           />
@@ -238,23 +262,27 @@ function DetailedGoalRecommendationBox({
 
   return (
     <div className="flex justify-center mt-6">
-      <div className="bg-primary/10 p-4 rounded-lg max-w-md">
-        <div className="flex items-start gap-3 mb-4">
-          <img src={koalaImage} alt="코알라" className="w-6 h-6 mt-1" />
-          <div className="text-sm">
-            <p className="font-medium text-primary mb-1">코알라 팁!</p>
-            <p className="text-primary/80">
-              각 세부 목표는 측정 가능하고 구체적으로 작성하세요. 예: "운동하기"
-              → "주 3회 30분 이상 조깅하기"
+      <div className="bg-primary/10 p-4 rounded-lg  max-w-md w-full">
+        <div className="md:px-[45px] flex items-start gap-3 mb-4">
+          {/* <img src={koalaImage} alt="코알라" className="w-6 h-6 mt-1" /> */}
+          <div className="text-[10px]">
+            <p className="text-base font-semibold text-primary mb-1">TIP!</p>
+            <p className="text-[#999999] flex flex-col">
+              <span className="flex-1">
+                각 세부 목표는 측정 가능하고 구체적으로 작성하세요.
+              </span>
+              <span className="flex-1">
+                ex) 운동하기 → 주 3회 30분 이상 조깅하기
+              </span>
             </p>
           </div>
         </div>
 
         {/* 목표 추천받기 버튼 */}
-        <div className="text-center">
+        <div className="text-center px-[57px]">
           <Button
             onClick={handleSuggestGoals}
-            className="pixel-button bg-primary/90 hover:bg-primary text-white px-4 py-2 text-sm w-full "
+            className="pixel-button border-1 border-[#A80D00] bg-[#FF1502] opacity-50 hover:bg-primary text-white py-2 text-sm w-full "
             disabled={isOnboardingOpen || !main?.content.trim() || isStreaming}
             data-tutorial="recommendation-button"
           >
@@ -265,14 +293,14 @@ function DetailedGoalRecommendationBox({
               </>
             ) : (
               <>
-                <Lightbulb className="h-4 w-4 mr-2" />
+                <RecommendIcon />
                 목표 추천받기
               </>
             )}
           </Button>
           {isStreaming && <Button onClick={stopStream}>취소하기</Button>}
           {!mainItems[0].content.trim() && (
-            <p className="text-xs text-gray-500 mt-2">
+            <p className="text-[9px] text-[#999999] mt-2">
               주요 목표를 먼저 입력해주세요
             </p>
           )}
