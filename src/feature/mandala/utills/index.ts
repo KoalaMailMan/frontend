8bc69a262b4ca1780f46106c9074c594e575ec6b
mamonde456 <mamonde456@gmail.com>
2025-12-15 15:24:33 +0900

    feat: 대시보드의 main index, sub index 찾는 유틸 함수 추가

--- src/feature/mandala/utills/index.ts
+++ src/feature/mandala/utills/index.ts
@@ -1,3 +1,4 @@
+import type { MainGoal } from "@/lib/stores/mandalaStore";
 import { IntervalType } from "../const";
 
 export const findKeyByValue = (type: string) => {
@@ -13,4 +14,20 @@ export const moveItem = <T>(arr: T[], from: number, to: number): T[] => {
 
   newArr.splice(to, 0, moved);
   return newArr;
+};
+
+export const findIdIndex = (data: MainGoal[], targetId: string) => {
+  for (let i = 0; i < data.length; i++) {
+    const main = data[i];
+
+    if (main.goalId === targetId) {
+      return { mainIndex: i, subIndex: null };
+    }
+
+    const subIndex = main.subs.findIndex((sub) => sub.goalId === targetId);
+    if (subIndex !== -1) {
+      return { mainIndex: i, subIndex };
+    }
+  }
+  return { mainIndex: -1, subIndex: -1 };
 };
