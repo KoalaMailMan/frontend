b98676a6a9cca7b00158ac1e5b0de4a2330ac78d
mamonde456 <mamonde456@gmail.com>
2025-12-16 10:26:12 +0900

    fix: unchecked background color 변경

--- src/feature/ui/Switch.tsx
+++ src/feature/ui/Switch.tsx
@@ -13,7 +13,7 @@ function Switch({
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "peer data-[state=checked]:bg-[#333333] data-[state=unchecked]:bg-[#333333] focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-[#333333]/80 inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "peer data-[state=checked]:bg-[#333333] data-[state=unchecked]:bg-[#E6E6E6] focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-[#333333]/80 inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
