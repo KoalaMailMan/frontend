312640a9e34e8b3eab37191bcc34b528c163f959
mamonde456 <mamonde456@gmail.com>
2026-01-12 14:13:54 +0900

    fix: image url / const obj 분리

---
+++ src/shared/hooks/const/theme.ts
@@ -0,0 +1,63 @@
+import { autumn_theme, spring_theme, summer_theme, winter_theme } from "./url";
+
+export const themeColors = {
+  purple: "#86569d",
+  red: "#ff5042",
+  blue: "#40bbed",
+  green: "#3aab63",
+  yellow: "#ffe849",
+  pink: "#e17aaa",
+  spring: "#FF99A9",
+  summer: "#02C2FE",
+  autumn: "#FA7018",
+  winter: "#3085BA",
+};
+export const modalThemeColors = {
+  purple: "#86569d",
+  red: "#df6556",
+  blue: "#40bbed",
+  green: "#3aab63",
+  yellow: "#ffe849",
+  pink: "#e17aaa",
+  spring: "#FF99A9",
+  summer: "#02C2FE",
+  autumn: "#FA7018",
+  winter: "#3085BA",
+};
+export const modalBorderThemeColors = {
+  red: "#a80d00",
+  purple: "#86569d",
+  blue: "#40bbed",
+  green: "#3aab63",
+  yellow: "#ffe849",
+  pink: "#e17aaa",
+  spring: "#FF99A9",
+  summer: "#02C2FE",
+  autumn: "#FA7018",
+  winter: "#3085BA",
+};
+export const recommendButtonThemeColors = {
+  red: "#ff7f75",
+  purple: "#86569d",
+  blue: "#40bbed",
+  green: "#3aab63",
+  yellow: "#ffe849",
+  pink: "#e17aaa",
+  spring: "#FF99A9",
+  summer: "#02C2FE",
+  autumn: "#FA7018",
+  winter: "#3085BA",
+};
+
+export const themeBackgrounds = {
+  spring: spring_theme.url,
+  summer: summer_theme.url,
+  autumn: autumn_theme.url,
+  winter: winter_theme.url,
+};
+export const themeBackgroundsSrcSet = {
+  spring: spring_theme.srcSet,
+  summer: summer_theme.srcSet,
+  autumn: autumn_theme.srcSet,
+  winter: winter_theme.srcSet,
+};
