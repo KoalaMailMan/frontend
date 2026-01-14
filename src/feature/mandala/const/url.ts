e5a75c747c97364bb33e64ec7012094cb9c4fd2b
mamonde456 <mamonde456@gmail.com>
2026-01-14 16:16:56 +0900

    fix: 이미지 최적화 및 이미지 url 분리

--- src/feature/mandala/const/url.ts
+++ src/feature/mandala/const/url.ts
@@ -2,8 +2,9 @@
 import KoalaLogo from "@/assets/common/header_text_logo.png?width=476;952;1428&format=webp";
 // @ts-expect-error -- vite-imagetools query import
 import KoalaLogoSrcSet from "@/assets/common/header_text_logo.png?width=476;952;1428&as=srcset&format=webp";
+
 // @ts-expect-error -- vite-imagetools query import
-import koala_seal from "@/assets/mandalart/koala_ seal.png&format=webp";
+import koala_seal from "@/assets/mandalart/koala_ seal.png?format=webp";
 
 export const KoalaTextLogo = KoalaLogo;
 export const KoalaTextLogoSrcSet = KoalaLogoSrcSet;
