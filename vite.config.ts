a07129fbeef083d9b2c49d7ef2529f0f16c1db74
mamonde456 <mamonde456@gmail.com>
2026-01-16 14:47:43 +0900

    feat: SEO를 위한 SSG/SSR 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import vike from "vike/plugin";
 import { defineConfig, loadEnv } from "vite";
 import { imagetools } from "vite-imagetools";
 
@@ -8,21 +9,12 @@ import { imagetools } from "vite-imagetools";
 export default ({ mode }: { mode: string }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return defineConfig({
-    build: {
-      minify: "esbuild",
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            "react-vendor": ["react", "react-dom"],
-          },
-        },
-      },
-    },
     esbuild: {
       drop: mode === "production" ? ["console", "debugger"] : [], // 프로덕션에서 console, debugger 제거
     },
     plugins: [
       react(),
+      vike(),
       tailwindcss(),
       imagetools({
         defaultDirectives: (url) => {
