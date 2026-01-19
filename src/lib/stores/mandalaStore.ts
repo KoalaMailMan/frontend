17b975f158e00f78fb0cbc52d19a52e02bc7d0f0
mamonde456 <mamonde456@gmail.com>
2026-01-19 15:07:31 +0900

    fix: 주요 목표 content가 작성되어야 모든 목표 완료 스탬프 활성화 조건 추가

--- src/lib/stores/mandalaStore.ts
+++ src/lib/stores/mandalaStore.ts
@@ -143,8 +143,8 @@ export const useMandalaStore = create<States & Actions>()(
             const isSubsComplete = mains[mainIndex].subs
               .slice(1)
               .every((item) => item.status === "DONE");
-
-            if (isSubsComplete) {
+            const isCenterTextWritten = mains[mainIndex].subs[0].content !== "";
+            if (isSubsComplete && isCenterTextWritten) {
               const { mainId, newMain } = toggleStatus(
                 "DONE",
                 mains,
