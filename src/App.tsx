dfa0ff62301fb889010d2c7c3599df25d4e8872c
mamonde456 <mamonde456@gmail.com>
2026-02-02 16:48:25 +0900

    fix: auth 로직 분리로 렌더링만 담당하도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import { useEffect } from "react";
-import {
-  handleLogin,
-  handleLogout,
-  reissueWithRefreshToken,
-  shouldAttemptRefresh,
-} from "./feature/auth/service";
 import { useAuthStore } from "./lib/stores/authStore";
-import MandalaBoard from "./feature/mandala/pages/MandalaBoard";
 import useTheme from "./shared/hooks/useTheme";
-import { handleMandalaData } from "./feature/mandala/service";
-import { APIWithRetry, getURLQuery } from "./feature/auth/\butils";
+import { clearQuery, getURLQuery } from "./feature/auth/\butils";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "./feature/ui/Sonner";
@@ -19,7 +11,7 @@ import { useTutorialStore } from "./lib/stores/tutorialStore";
 import OnboardingTutorial from "./feature/tutorial/page";
 import useResize from "./shared/hooks/useResize";
 import AuthComponent from "./feature/auth/components/AuthComponent";
-import HomePage from "./feature/home/pages/HomePage";
+import AuthEntry from "./feature/auth/components/\bAuthGateway";
 
 const queryClient = new QueryClient();
 
@@ -28,42 +20,27 @@ function App() {
   const { currentTheme, updateCurrentTheme, getCurrentBackground } = useTheme();
   const wasLoggedIn = useAuthStore((state) => state.wasLoggedIn);
   const isAuthOpen = useAuthStore((state) => state.isAuthOpen);
-  const temporaryAuth = useAuthStore((state) => state.temporaryAuth);
   const authComponentText = useAuthStore((state) => state.authComponentText);
   const isOnboardingOpen = useTutorialStore((state) => state.isOnboardingOpen);
+
+  const setAccessToken = useAuthStore((state) => state.setAccessToken);
   const setTemporaryAuth = useAuthStore((state) => state.setTemporaryAuth);
 
   useEffect(() => {
     const initApp = async () => {
       try {
         // 최초 로그인
+        const token = getURLQuery("access_token");
         const errorFromUrl = getURLQuery("error");
         if (errorFromUrl) {
           toast.error("로그인에 실패했습니다. 다시 시도해주세요.");
-          window.history.replaceState(
-            {},
-            document.title,
-            window.location.pathname
-          );
+          clearQuery();
           return;
         }
-        const tokenFromURL = getURLQuery("access_token");
-        if (tokenFromURL) {
-          handleLogin();
-          await handleMandalaData();
+        if (token) {
+          setAccessToken(token);
+          clearQuery();
           return;
-        }
-
-        // 새로고침 / 재접속이라면 refresh 시도
-        if (shouldAttemptRefresh()) {
-          const success = await APIWithRetry(reissueWithRefreshToken);
-          if (!success) {
-            handleLogout();
-            toast("세션 종료로 인해 처음 화면으로 돌아갑니다.");
-            return;
-          }
-          setTemporaryAuth("loggedIn");
-          await handleMandalaData();
         }
       } catch (error) {
         console.error("Auth initialization failed:", error);
@@ -77,11 +54,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Header currentTheme={currentTheme} onThemeChange={updateCurrentTheme} />
 
-      {temporaryAuth !== "none" || wasLoggedIn ? (
-        <MandalaBoard getCurrentBackground={getCurrentBackground} />
-      ) : (
-        <HomePage getCurrentBackground={getCurrentBackground} />
-      )}
+      <AuthEntry getCurrentBackground={getCurrentBackground} />
 
       <Toaster position="top-center" />
       {isOnboardingOpen && <OnboardingTutorial />}
