5397b557bba80cdeee0781abc47c6e9fb3357701
mamonde456 <mamonde456@gmail.com>
2026-03-09 17:02:57 +0900

    chore: vike 환경 설정
    - 에러 페이지 추가
    - main.tsx 삭제 및 App 컴포넌트 렌더
    - css 테마 기본 선택 스크립트 추가
    - vike-react 환경 설정

---
+++ src/pages/_error/+Page.tsx
@@ -0,0 +1,7 @@
+export default function ErrorPage() {
+  return (
+    <div>
+      <h1>에러가 발생했습니다</h1>
+    </div>
+  );
+}
---
+++ src/pages/index/+Page.tsx
@@ -0,0 +1,7 @@
+import App from "@/App";
+import "../../styles/globals.css";
+import "../../styles/loading_ui.css";
+
+export default function Page() {
+  return <App />;
+}
