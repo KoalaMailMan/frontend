d69162a81f499f4efb75dd2602c5205d79292224
mamonde456 <mamonde456@gmail.com>
2026-03-27 14:41:25 +0900

    fix: mandalart return error 추가

--- src/feature/mandala/api/mandalart/createMandala.ts
+++ src/feature/mandala/api/mandalart/createMandala.ts
@@ -4,12 +4,13 @@ import type { ServerMandalaType } from "../../service";
 export const createMandalaAPI = async (data: ServerMandalaType) => {
   const MANDALA_URL = "/api/mandalart";
   try {
-    const res = await apiClient.put(MANDALA_URL, data, {
+    const res = await apiClient.put<ServerMandalaType>(MANDALA_URL, data, {
       requiresAuth: true,
       credentials: "include",
     });
     return res;
   } catch (error) {
     console.error("createMandalaAPI failed:", error);
+    throw new Error("createMandalaAPI: 응답이 없습니다.");
   }
 };
