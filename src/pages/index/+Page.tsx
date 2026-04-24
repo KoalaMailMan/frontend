1cf3e2a1244cf434786568661f9611a99cef7371
mamonde456 <mamonde456@gmail.com>
2026-04-24 10:27:19 +0900

    chore: immer 환경 세팅
    - immer 에서 Map,Set 지원하도록 세팅

--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -1,6 +1,9 @@
 import App from "@/App";
 import "../../styles/globals.css";
 import "../../styles/loading_ui.css";
+import { enableMapSet } from "immer";
+
+enableMapSet();
 
 export default function Page() {
   return <App />;
