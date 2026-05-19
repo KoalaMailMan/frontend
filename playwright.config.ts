adc1da0edbae616e214864cd371ae4f74057c0e5
mamonde456 <mamonde456@gmail.com>
2026-05-19 19:46:12 +0900

    fix: http > https 로 변경

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: "https://localhost:3000",
+    ignoreHTTPSErrors: true,
     actionTimeout: 10000,
     navigationTimeout: 10000,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -74,7 +75,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    url: "http://localhost:3000",
+    url: "https://localhost:3000",
     reuseExistingServer: !process.env.CI,
+    ignoreHTTPSErrors: true,
   },
 });
