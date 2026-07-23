e2e513d0a56d9e28de501e82c5443113a9564453
mamonde456 <mamonde456@gmail.com>
2026-07-23 09:53:31 +0900

    chore/test: fixtures 데이터 추가

--- src/lib/stores/__tests__/fixtures/serverMandala.ts
+++ src/lib/stores/__tests__/fixtures/serverMandala.ts
@@ -19,6 +19,33 @@ export const createServerMandalaFixture = () => ({
             },
           ],
         },
+        {
+          goalId: 4,
+          position: 3,
+          content: "운동",
+          status: "UNDONE" as "DONE" | "UNDONE",
+          subs: [
+            {
+              goalId: 5,
+              position: 1,
+              content: "30분 근력운동",
+              status: "UNDONE" as "DONE" | "UNDONE",
+            },
+            {
+              goalId: 6,
+              position: 3,
+              content: "30분 유산소",
+              status: "UNDONE" as "DONE" | "UNDONE",
+            },
+          ],
+        },
+        {
+          goalId: 0,
+          position: 2,
+          content: "영화",
+          status: "UNDONE" as "DONE" | "UNDONE",
+          subs: [],
+        },
       ],
     },
   },
